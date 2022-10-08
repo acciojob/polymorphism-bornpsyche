cc52f27ba3ffda8f0d4df06793bbb8ca67cec92b
Ashish Pradhan <bornpsyche@gmail.com>
2022-10-08 18:16:11 +0530

    1st commit: updated Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    Product p = new Product();
+    int x = p.product(1, 2);
+    int y = p.product(1, 2, 3);
+    double z = p.product(1.0, 2.0);
+}
 
+class Product {
+    int product(int x, int y) {
+        return x * y;
+    }
+
+    int product(int x, int y, int z) {
+        return x * y * z;
+    }
+
+    double product(double x, double y) {
+        return x * y;
+    }
 }
